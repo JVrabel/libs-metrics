5390e9f2ca5eca43adeb2b69e77ab76923d1f26b
Pavel Nedelnik <p.nedelnik@email.cz>
2022-12-09 17:25:45 +0100

    initial

--- src/simulation.py
+++ src/simulation.py
@@ -4,7 +4,7 @@ def get_spectrum(return_wavelength=False, *args, **kwargs):
     spectrum = simulation.SimulatedLIBS(
         *args,
         **kwargs,
-    ).get_interpolated_spectrum()
+    ).get_raw_spectrum()
     if return_wavelength:
         return spectrum['intensity'].to_numpy(), spectrum['wavelength'].to_numpy()
     return spectrum['intensity'].to_numpy()
